d15429faeeb53e32271fa6e26d6c4bb95cc08969
Elisabeth <m.e.elisa@yandex.ru>
2023-03-06 22:25:15 +0300

    Решение задачи 6

---
+++ Task_06/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка (10-99) и показывает наибольшую цифру числа
+// 78->8
+// 12->2
+// 85->8
+
+Random rand = new Random();
+int randNumber = rand.Next(10,100);  // 1,2,3....99  [10,100) от 10 до 99 включительно, 100 не включается
+
+System.Console.WriteLine(randNumber);
+
+int leftNumber = randNumber / 10;
+int rightNumber = randNumber % 10;
+
+if(leftNumber>rightNumber)
+{
+    System.Console.WriteLine($"{leftNumber} > {rightNumber}");
+}
+else if(rightNumber>leftNumber)
+{
+    System.Console.WriteLine($"{rightNumber} > {leftNumber}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
