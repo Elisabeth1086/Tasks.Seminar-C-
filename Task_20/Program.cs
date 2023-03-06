e8a6492ef6d37e8a8bc8132a5c0483f1bf270472
Elisabeth <m.e.elisa@yandex.ru>
2023-03-06 22:59:04 +0300

    Решение задачи 20

---
+++ Task_20/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 32. Напишите программу замены элементов массива: положительные заменить на соответствующие отрицательные и наоборот
+// [-4,-8,8,2] - [4,8,-,8-2]
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void FlipNegative(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+
+int[] array = GenerateArray(10, -9, 9);
+PrintArray(array);
+FlipNegative(array);
+PrintArray(array);
