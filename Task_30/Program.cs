df79c4cc7823b5c3032abbc3c5f5554a768890b7
Elisabeth <m.e.elisa@yandex.ru>
2023-03-13 01:07:47 +0300

    Решение задачи 30

---
+++ Task_30/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле A(mn)=m+n
+// Выведите полученный массив
+// 0123
+// 1234
+// 2345
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
