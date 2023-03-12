2a9cd52055300789825935ef52658da10b2a7256
Elisabeth <m.e.elisa@yandex.ru>
2023-03-12 18:21:47 +0300

    Решение задачи 26 рекурсия

---
+++ Task_26_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 44 - 101100
+// 3 - 11
+// 2 - 10
+// 2 способ (рекурсия)
+
+int ReadInt(string promt)
+{
+    System.Console.WriteLine(promt);                      //Приняли число
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetBinaryView(int num)                                //рекурсия
+{
+    if (num == 0) return;
+    GetBinaryView(num / 2);
+    System.Console.Write(num % 2);
+}
+
+int N = ReadInt("Введите число: ");
+GetBinaryView(N);
