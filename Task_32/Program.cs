9deb11e3eb5438ccc94cf336e04c658601519306
Elisabeth <m.e.elisa@yandex.ru>
2023-03-13 02:04:28 +0300

    Решение задачи 32

---
+++ Task_32/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 51. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали с индексами (0,0) (1,1) и тд.
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов диагонали - 1+9+2=12
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(-10, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int Number(int[,] matrix)
+{
+    int number = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        number += matrix[i, i];
+    }
+    return number;
+}
+
+
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine($"Сумма элементов равна {Number(myMatrix)}");
