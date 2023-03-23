e95bbf407ed674da537e0ae662adc8c046da65e8
Elisabeth <m.e.elisa@yandex.ru>
2023-03-23 18:30:45 +0300

    Решение задачи 37

---
+++ Task_37/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N=5 - 1,2,3,4,5
+// N=6 - 1,2,3,4,5,6
+
+int ReadInt(string prompt)
+{
+    System.Console.WriteLine(prompt);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteNumbers(int num)
+{
+    if (num == 0) return;
+    WriteNumbers(num - 1);
+    System.Console.Write(num + " ");
+}
+
+WriteNumbers(ReadInt("Введите число: "));
